ddeb89e1a0488d3d6b0b781d91aa15d4138d8c73
kws970609 <kws970609@users.noreply.github.com>
2020-03-19 12:29:07 +0900

    완성

--- app/src/main/java/kr/co/tjoeun/a20200319_fragment01/fragments/SecondFragment.java
+++ app/src/main/java/kr/co/tjoeun/a20200319_fragment01/fragments/SecondFragment.java
@@ -3,11 +3,15 @@ package kr.co.tjoeun.a20200319_fragment01.fragments;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.databinding.ViewStubProxy;
 import androidx.fragment.app.Fragment;
 
 import kr.co.tjoeun.a20200319_fragment01.R;
@@ -23,20 +27,43 @@ public class SecondFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_second,container,false);
         return binding.getRoot();
+
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
-            }
-        });
+                String id = binding.idEdt.getText().toString();
+                String pw = binding.pwEdt.getText().toString();
 
+                if (id.equals("admin") && pw.equals("asdf1234")) {
+
+                    Toast.makeText(getActivity(), "관리자 로그인", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if(id.equals("admin")) {
+//                        ID는 맞게 입력
+                            Toast.makeText(getActivity(), "비번이 틀렸습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+//                        ID 부터 잘못 입력
+                            Toast.makeText(getActivity(), "잘못된 아이디입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+
+
+
+                }
+
+        });
 
 
     }
