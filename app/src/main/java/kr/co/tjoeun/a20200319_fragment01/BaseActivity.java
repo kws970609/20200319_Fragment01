8dc5e87b34554a57af098a57ec155792da8e24c9
kws970609 <kws970609@users.noreply.github.com>
2020-03-19 09:41:52 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200319_fragment01/BaseActivity.java
@@ -0,0 +1,15 @@
+package kr.co.tjoeun.a20200319_fragment01;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+}
