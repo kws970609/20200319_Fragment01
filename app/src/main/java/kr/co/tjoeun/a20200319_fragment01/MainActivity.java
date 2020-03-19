841bd72bc8145b895016842d908f73dd687b48e2
kws970609 <kws970609@users.noreply.github.com>
2020-03-19 10:42:03 +0900

    액티비티에서 프래그먼트 보여주고 /숨김

--- app/src/main/java/kr/co/tjoeun/a20200319_fragment01/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200319_fragment01/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import kr.co.tjoeun.a20200319_fragment01.databinding.ActivityMainBinding;
 
@@ -21,6 +22,31 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        binding.firstFragCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                첫 프래그먼트 호출 버튼이 눌리면?
+//                firstFrag 보이게 => firstFrag자체는 기능이 별로 없다.
+//                firstFrag를 감싸는 레이아웃을 추가 => 애를 보이게함.
+                binding.firstFragLayout.setVisibility(View.VISIBLE);
+//                secondFrag 숨김.
+                binding.secondFragLayout.setVisibility(View.GONE);
+
+            }
+        });
+
+        binding.secondFragCallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                첫 프래그 : 숨겨줌
+                binding.firstFragLayout.setVisibility(View.GONE);
+//                둘째 프래그 : 나타냄
+                binding.secondFragLayout.setVisibility(View.VISIBLE);
+
+            }
+        });
 
     }
 
